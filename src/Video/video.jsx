b81f2b004ae87855aca8be01c9edf84e22216cc7
IvanSemenkin <ismenkin@gmail.com>
2024-12-30 12:10:23 +0300

    Добавлена кнопка удалить лайк

--- src/Video/video.jsx
+++ src/Video/video.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react';
-import './video.css'
+import React, { useState } from 'react';
+import './video.css';
 
 function Video(props) {
     const { title, chanelName, img } = props;
     const [likesCount, setLikesCount] = useState(0);
 
-    console.log(title);
-    
     const increaseLikeByOne = () => {
         setLikesCount(likesCount + 1);
     }
+
+    const decreaseLikeByOne = () => {
+        setLikesCount(likesCount - 1);
+    }
+
     return (
         <div className='video'>
             <img src={img} alt="" />
@@ -18,6 +21,7 @@ function Video(props) {
             <div className='video-footer'>
                 <p>Лайки: {likesCount}</p>
                 <button onClick={increaseLikeByOne}>Лайк</button>
+                <button onClick={decreaseLikeByOne}>Удалить</button>
             </div>
         </div>
     );
